b4e150b328e3a5be35221d6c6e2000944448fc48
huashi33 <huashi33@foxmail.com>
2024-12-07 21:06:00 +0800

    add func add

---
+++ func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+
+inline int func_add(int a,int b){
+    return a+b;
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include "func.h"
 int main(int argc,char **argv){
     printf("hello world\n");
     printf("printf for second commit\n");
 
     printtf("for develop\n");
+
+    printf("2+3=%d\n",func_add(2,3));
 }
